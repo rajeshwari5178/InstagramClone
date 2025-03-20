ae33a0691ed388c629b829757e55cad8113f475b
Rajeshwari <rajshri.ok@gmail.com>
2025-03-20 16:14:40 +0530

    initial commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Rajeshwari on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
